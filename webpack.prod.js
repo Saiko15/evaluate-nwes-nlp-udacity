b2ece54b30f1d68d9f6507c3451f0e2222924651
Saiko15 <ahmedsaiko42@gmail.com>
2021-04-20 18:16:53 +0200

    6

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,7 +16,11 @@ module.exports = {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
-        ]
+        ],
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     },
     plugins: [
         new HtmlWebPackPlugin({
