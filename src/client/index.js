58d7a78146fd1ed7ddb6e63ee336c126c9189637
Saiko15 <ahmedsaiko42@gmail.com>
2021-04-20 18:30:42 +0200

    10

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,11 @@ console.log(checkForName);
 alert("I EXIST")
 console.log("CHANGE!!");
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
+import 'styles/resets.scss'
+import 'styles/base.scss'
+import 'styles/footer.scss'
+import 'styles/form.scss'
+import 'styles/header.scss'
 
 export {
     checkForName,
