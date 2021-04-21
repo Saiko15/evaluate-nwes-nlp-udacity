a8d58cd7378290860cc0eb710bc90abfba770bce
Saiko15 <ahmedsaiko42@gmail.com>
2021-04-21 10:01:25 +0200

    19

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+// alert("I EXIST")
+// console.log("CHANGE!!");
 
 import './styles/resets.scss'
 import './styles/base.scss'
